79d4dd69637295c976f036846da79662c8375334
Ryan Sylvestre <ryansylvestre@hotmail.com>
2021-11-05 12:14:16 -0700

    Scroll bug

--- client/src/components/Loading/LoadingModal.tsx
+++ client/src/components/Loading/LoadingModal.tsx
@@ -1,49 +1,43 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { Modal, ModalBody } from 'reactstrap';
 import { FaSpinner } from 'react-icons/fa';
 import LoadingService from './LoadingService';
 
-interface IMyState {
-  isOpen: boolean;
-  text: string;
-}
+export const LoadingModal: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [text, setText] = useState('');
 
-export class LoadingModal extends React.Component<{}, IMyState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      isOpen: false,
-      text: '',
-    };
-  }
-
-  public componentDidMount() {
-    LoadingService.register(this.open, this.close);
-  }
-
-  public componentWillUnmount() {
-    LoadingService.unregister(); // necessary?
-  }
-
-  public render() {
-    return (
-      <Modal isOpen={this.state.isOpen} size={'sm'}>
-        <ModalBody className={'text-center'}>
-          <FaSpinner size={'48px'} className={'spinner p-2'} />
-          <div>{this.state.text}</div>
-        </ModalBody>
-      </Modal>
-    );
-  }
-
-  private open = (text: string) => {
+  const open = (text: string) => {
     if (!text || text === '') {
       text = 'Loading...';
     }
-    this.setState({ text, isOpen: true });
+    setText(text);
+    setIsOpen(true);
   };
 
-  private close = () => {
-    this.setState({ isOpen: false });
+  const close = () => {
+    setIsOpen(false);
   };
-}
+
+  useEffect(() => {
+    LoadingService.register(open, close);
+    return () => setIsOpen(false);
+  }, []);
+
+  useEffect(() => {
+    if (!isOpen) {
+      document.querySelector('body')?.classList.remove('modal-open');
+      document.querySelector('body')?.setAttribute('style', '');
+    }
+  }, [isOpen]);
+
+  return (
+    <Modal isOpen={isOpen} size={'sm'}>
+      <ModalBody className={'text-center'}>
+        <FaSpinner size={'48px'} className='spinner' />
+        <div>{text}</div>
+      </ModalBody>
+    </Modal>
+  );
+};
