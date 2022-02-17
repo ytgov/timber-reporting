bdd0d61f251aa3b13769de49105c9656fedae6eb
Ryan Sylvestre <ryansylvestre@hotmail.com>
2022-02-17 09:46:11 -0700

    Navbar banner alignment

--- client/src/components/AuroraNavBar/AuroraNavbar.tsx
+++ client/src/components/AuroraNavBar/AuroraNavbar.tsx
@@ -1,32 +1,40 @@
 import * as React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import aurora from '../../aurora.svg';
+import { useWindowDimensions } from 'hooks/WindowHooks';
 
 export const AuroraNavBar: React.FC = (props) => {
+  const { xs, sm, md } = useWindowDimensions();
+  const hideBanner = xs || sm;
+
   return (
     <Navbar color={'light'} light={true} expand={'xs'} style={{ borderTop: '5px solid #ffcd57', overflow: 'hidden' }}>
-      <NavbarBrand className={'mr-auto media-padding d-flex'} style={{ flex:1,height: 100 }}>
-        <div style={{
+      <NavbarBrand className={'mr-auto media-padding d-flex'} style={{ flex: 1, height: 100 }}>
+        <div
+          style={{
             textAlign: 'center',
             flex: 5,
-            justifyContent:"flex-start",
+            justifyContent: 'flex-start',
             bottom: '13px',
             height: 100,
-
-        }}>  {props.children}</div>
-        {/*<div style={{ width: 350 }} className={'d-none d-md-block'}>*/}
+          }}
+        >
+          {' '}
+          {props.children}
+        </div>
+        {!hideBanner && (
           <div
             style={{
-              flex:1,
+              flex: 1,
               bottom: '13px',
               position: 'absolute',
-              right: -250,
+              right: md ? -350 : -250,
               height: 100,
             }}
           >
             <img src={aurora} style={{ height: '100%' }} alt={'aurora'} />
           </div>
-        {/*</div>*/}
+        )}
       </NavbarBrand>
     </Navbar>
   );
